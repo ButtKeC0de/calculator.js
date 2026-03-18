0a44680965207bc53d45a20d29e6332afc5f0e90
ButtKeC0de <Carlos_buttke@estudante.sesisenai.org.br>
2026-03-18 09:46:18 -0300

    ADD- FINISHIED

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-console.log("Calculadora Online :")
-let condicao = 1
-    while(condicao === 1) {
+console.log("Calculadora Online:");
 
+const disp = document.getElementById("resultado");
 
-
+function inserir(num) {
+    if (disp.textContent === "0") {
+        disp.textContent = num;
+    } else {
+        disp.textContent += num;
     }
+}
+
+function limpar() {
+    disp.textContent = "0";
+}
+
+function apagar() {
+    let atual = disp.textContent;
+    disp.textContent = atual.substring(0, atual.length - 1);
+
+    if (disp.textContent === "") {
+        disp.textContent = "0";
+    }
+}
+
+function calcular() {
+    let resultado = disp.textContent;
+
+    if (resultado) {
+        try {
+            disp.textContent = eval(resultado);
+        } catch (e) {
+            disp.textContent = "Erro";
+            setTimeout(limpar, 1500);
+        }
+    }
+}
